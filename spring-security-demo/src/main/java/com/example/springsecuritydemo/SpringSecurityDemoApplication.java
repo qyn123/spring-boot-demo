8d88fb59dfd55fe530e4083850571e0a9b3b1f96
qiaoyanan <yanan.qiao@lejian.com>
2022-10-07 20:14:58 +0800

    spring-security

---
+++ spring-security-demo/src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiaoyanan
+ */
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    }
+
+}
