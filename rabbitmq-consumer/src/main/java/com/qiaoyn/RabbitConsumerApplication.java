64b77dd3f9eade8ef11c1d135a6b359359c7ce5f
qiaoyanan <yanan.qiao@lejian.com>
2022-10-18 15:24:00 +0800

    rabbit-mq学习

---
+++ rabbitmq-consumer/src/main/java/com/qiaoyn/RabbitConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.qiaoyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiaoyanan
+ * date:2022/10/17 15:09
+ */
+@SpringBootApplication
+public class RabbitConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitConsumerApplication.class,args);
+    }
+}
---
+++ rabbitmq-provider/src/main/java/com/qiaoyn/RabbitProviderApplication.java
@@ -0,0 +1,15 @@
+package com.qiaoyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiaoyanan
+ * date:2022/10/17 15:02
+ */
+@SpringBootApplication
+public class RabbitProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitProviderApplication.class,args);
+    }
+}
