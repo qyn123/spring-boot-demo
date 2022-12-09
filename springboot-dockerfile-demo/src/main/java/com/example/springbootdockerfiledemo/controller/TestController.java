629b9241aef1f05975f53e58d99b936b5b3b80c0
qiaoyanan <yanan.qiao@lejian.com>
2022-12-09 13:48:07 +0800

    springboot docker 镜像

--- springboot-dockerfile-demo/src/main/java/com/example/springbootdockerfiledemo/controller/TestController.java
+++ springboot-dockerfile-demo/src/main/java/com/example/springbootdockerfiledemo/controller/TestController.java
@@ -3,6 +3,11 @@ package com.example.springbootdockerfiledemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
 /**
  * @author qiaoyanan
  * date:2022/11/17 10:41
@@ -13,7 +18,13 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello,docker ~";
+        try {
+            InetAddress host = Inet4Address.getLocalHost();
+            return "hello," + host.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "hello,docker~~";
     }
 
 }
